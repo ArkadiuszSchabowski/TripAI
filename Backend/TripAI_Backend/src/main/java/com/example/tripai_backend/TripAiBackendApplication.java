d031c9fae91366dc696781c3d8851b68400ae513
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-04-11 11:30:47 +0200

    feat: add backend project setup

---
+++ Backend/TripAI_Backend/src/main/java/com/example/tripai_backend/TripAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripAiBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/TripAI_Backend/src/test/java/com/example/tripai_backend/TripAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
