e234e719c56e4a9f321bc786d642798d45d9aa86
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-05-15 19:53:39 +0200

    test: test deployment

--- Backend/TripAI_Backend/src/main/java/com/example/tripai_backend/TripAiBackendApplication.java
+++ Backend/TripAI_Backend/src/main/java/com/example/tripai_backend/TripAiBackendApplication.java
@@ -3,7 +3,7 @@ package com.example.tripai_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Test pipeline
+//Test deployment
 
 @SpringBootApplication
 public class TripAiBackendApplication {
