d2cb3c882ece3433e46d8f05441b17b46e3a9432
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-05-17 22:34:20 +0200

    create itinerary component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/public/home/home.component';
+import { ItineraryComponent } from './components/public/itinerary/itinerary.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent}
+  {path: "", component: HomeComponent},
+  {path: "itinerary", component: ItineraryComponent}
 ];
 
 @NgModule({
---
+++ Client/src/app/components/public/itinerary/itinerary.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { TripStateService } from '../../../_services/trip-state.service';
+import { TripAgentResponse } from '../../../models/trip-agent-response';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-itinerary',
+  templateUrl: './itinerary.component.html',
+  styleUrls: ['./itinerary.component.scss'],
+})
+export class ItineraryComponent {
+  tripAgentResponse: TripAgentResponse | null = null;
+
+  constructor(private router: Router, private tripStateService: TripStateService) {}
+
+  ngOnInit() {
+    this.tripStateService.tripData$.subscribe((data) => {
+      if (data) {
+        this.tripAgentResponse = data;
+      }
+    });
+  }
+  backToHome() {
+        this.tripStateService.setTripData(null);
+        this.router.navigateByUrl('/');
+  }
+}
