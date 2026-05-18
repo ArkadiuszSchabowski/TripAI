0a91f11816f73db64db3e4f95dca3f3dd8f8179c
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-05-18 20:08:46 +0200

    feat: create navbar with component navigation

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -11,9 +11,10 @@ import { ToastrModule } from 'ngx-toastr';
 import { MaterialModule } from './modules/material/material.module';
 import { ErrorInterceptor } from './interceptors/error-interceptor.interceptor';
 import { ItineraryComponent } from './components/public/itinerary/itinerary.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ItineraryComponent],
+  declarations: [AppComponent, HomeComponent, ItineraryComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- Client/src/app/modules/material/material.module.ts
+++ Client/src/app/modules/material/material.module.ts
@@ -5,13 +5,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    
     MatButtonModule,
     MatCardModule,
     MatDatepickerModule,
@@ -25,8 +29,11 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatCardModule,
     MatDatepickerModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
-    MatNativeDateModule
+    MatMenuModule,
+    MatNativeDateModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
