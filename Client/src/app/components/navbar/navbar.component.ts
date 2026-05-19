7975a928d2dfac699865edc1e54739b49f0a5b45
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-05-19 17:35:49 +0200

    feat: update navbar (display buttons for agent response and without agent response)

--- Client/src/app/components/navbar/navbar.component.ts
+++ Client/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TripStateService } from '../../_services/trip-state.service';
+import { TripAgentResponse } from '../../models/trip-agent-response';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  tripAgentResponse: TripAgentResponse | null = null;
   constructor(
     private router: Router,
+    private tripStateService: TripStateService,
   ) {}
+  ngOnInit(): void {
+    this.tripStateService.tripData$.subscribe({
+      next: (response) => {
+        ((this.tripAgentResponse = response), console.log(response));
+      },
+    });
+  }
 
   get isHomePage(): boolean {
     return this.router.url === '/';
