e69afd3d412294a386b3c7c15b4b7e87cd91aa78
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-05-20 19:54:58 +0200

    test: create integration test for city validation service

---
+++ Backend/TripAI_Backend/src/test/java/com/example/tripai_backend/integration/CityValidationServiceIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.example.tripai_backend.integration;
+
+import com.example.tripai_backend.service.CityValidationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class CityValidationServiceIntegrationTest {
+    @Autowired
+    CityValidationService service;
+
+    @ParameterizedTest
+    @CsvSource({
+            "London, true",
+            "LONDIN, false",
+            "Paris, true",
+            "Pari, false",
+            "Madrid, true",
+            "Madrit, false",
+            "TOKYO, true",
+            "TOKIO, false",
+            "BARCELONA, true",
+            "nonexistentcity, false",
+    })
+    void exists_whenCalled_shouldValidateCities(String city, boolean expected) {
+        assertEquals(expected, service.exists(city));
+    }
+}
